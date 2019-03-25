e7d27ae8ddf113c06cae44d34779181785a2a35c
Scott Timmermans <scott.timmermans@gmail.com>
2019-03-25 15:50:14 +0100

    sensor class  file
    needs to be declared in the header file but SANDER IS NOT PUSHING THE DEVELOP

--- sensor.cpp
+++ sensor.cpp
@@ -5,11 +5,42 @@ class sensors
 {
     public:
 
-    int black;
-    int white;
+    int black, white average;
+    int lightValue;
+
+    sensors(){
+        BP.set_sensor_type(PORT_3, SENSOR_TYPE_NXT_LIGHT_ON);
+        BP.set_sensor_type(PORT_1, SENSOR_TYPE_TOUCH);
+        sensor_light_t Light3;
+        sensor_touch_t Touch1;
+    }
+
+    void update(){
+        BP.get_sensor(PORT_3, &Light3);
+        lightValue = Light3.reflected;
+    }
     
+    void calibrate(){
 
+        while (true){
+            // wait for user input (druk sensor Nxt)
+            if (BP.get_sensor(PORT_1, &Touch1)) {
+                update();
+                white = lightValue;
+                break;
+            }  
+        }
+        
+        while (true){
+        // wait for user input (druk sensor Nxt)
+            if (BP.get_sensor(PORT_1, &Touch1)) {
+                update();
+                black = lightValue;
+                break;
+            }
+        }
+        average = black+((white-black)/2);
+        printf(average);
+    }
 
-
-
-}
+};
