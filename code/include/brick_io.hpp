2989f438cb3c5275378e34e5b1c34917f5c9250d
Scott Timmermans <scott.timmermans@gmail.com>
2019-03-27 13:50:22 +0100

    new tasks, new hierarchy, new io file name (brick_io.cpp) new scripts

---
+++ code/include/brick_io.hpp
@@ -0,0 +1,46 @@
+#ifndef BRICK_IO_HPP
+#define BRICK_IO_HPP
+
+#include "../include/BrickPi3.h"
+
+class IO {
+
+   public:
+    // Light Sensor variables
+    int black = 2200;
+    int white = 1200;
+    float average = 1700;
+    int lightValue;
+    // Ultra-Sonic-Sensor Variables
+    int distance;
+    // Touch Sensors Varables
+    bool touchSensor1, touchSensor2;
+
+    // Sensor write variables
+    sensor_touch_t Touch1, Touch2;
+    sensor_light_t Light3;
+    sensor_ultrasonic_t Ultrasonic4;
+
+    // Motor Variables
+    bool drive = true;
+
+    int MAX_SPEED = 370;
+
+    BrickPi3 BP;
+
+    IO();
+
+    void update();
+    
+    void calibrate();
+
+    int calcSpeed();
+
+    void resetEncoders();
+
+    void dpsA(int speed);
+    void dpsB(int speed);
+    void dpsC(int speed);
+};
+
+#endif
---
+++ code/include/controller.hpp
@@ -0,0 +1,34 @@
+#ifndef CONTROLLER_HPP
+#define CONTROLLER_HPP
+
+#include <fcntl.h>
+#include <unistd.h>
+#include <bitset>
+#include <fstream>
+#include <iostream>
+
+struct js_event {
+    uint32_t buttons;
+    int32_t joyX;
+    uint8_t joyY;
+    uint8_t uselessStuff;
+};
+
+class classControl {
+   public:
+    bool a, b, x, y;
+    bool lb, rb;
+    bool home, back, start;
+    bool bjl, bjr;
+    int joyX, joyY;
+    int fd;
+    struct js_event e;
+
+    classControl();
+
+    void update();
+
+    void printInput();
+};
+
+#endif
