efb5507b5ec08b8759356db6144fa5d760fd5eb9
Scott Timmermans <scott.timmermans@gmail.com>
2019-04-02 14:25:04 +0200

    controller for linux files are now in a different folder

---
+++ linuxCXBOX/src/main.cpp
@@ -0,0 +1,18 @@
+/*
+Authors: Sander Boot, Scott Timmermans, Tobias van den Hoogen, Mike Hoogendoorn,
+Ruben Zwietering
+
+*/
+#include <signal.h>  // for catching exit signals
+#include <stdio.h>   // for printf
+#include <unistd.h>  // for usleep
+
+// Class headers
+#include "../include/controller.hpp"  // Controller input Class
+
+int main() {
+    
+
+    
+}
+
